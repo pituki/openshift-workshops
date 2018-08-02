9a1589906e8901662cb2c192ea1fec4a1c30dce9
Alex van <pitukia@gmail.com>
2018-08-02 11:15:57 -0400

    added console log

--- dc-metro-map/app.js
+++ dc-metro-map/app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
 var app = express();
-
+console.log("app");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
